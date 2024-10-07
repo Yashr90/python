466b38245da373c9075c6e5956e53e433fbe806c
yash raj <159118494+Yashr90@users.noreply.github.com>
2024-10-07 10:49:26 +0530

    Add files via upload

---
+++ a1.py
@@ -0,0 +1,33 @@
+import os
+print("hello alliance")
+str = "hello world"
+print(str[::-2])
+
+print(str[:-2])
+
+print(str[2::])
+
+print(str[::2])
+
+print(str[::-4])
+
+print(str[0:11:2])
+
+print(str[::3])
+
+print(str[:5])
+a="hello, alliance!!"
+print(a.strip())
+print(a.lower())
+print(a.upper())
+print(len(a))
+print(str.count("w"))
+print(str.index("w"))
+
+a=20
+b=30
+a,b=b,a
+print(a)
+print(b)
+
+
---
+++ a11.py
@@ -0,0 +1,9 @@
+n = float(input())
+if(n>=60):
+    print("first division")
+elif(n>=50):
+    print("second divsion")
+elif(n>=40):
+    print("third division")
+else:
+    print("fail")
